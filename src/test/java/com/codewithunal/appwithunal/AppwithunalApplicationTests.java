231d44a5c05fe2a09e710b8435c89658e9ccfb2b
unalfocadan <unalfocadan@gmail.com>
2024-06-08 00:15:37 +0300

    Initial commit with project structure and setup

---
+++ src/test/java/com/codewithunal/appwithunal/AppwithunalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithunal.appwithunal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppwithunalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
