52c675c2fe85b14bb2bc032c063afe6e382ea597
Unal Focadan <unal.focadan@gmail.com>
2024-07-16 00:07:15 +0300

    16072024_commit

--- src/main/java/com/codewithunal/appwithunal/AppwithunalApplication.java
+++ src/main/java/com/codewithunal/appwithunal/AppwithunalApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @SpringBootApplication
 @RestController
@@ -14,9 +18,26 @@ public class AppwithunalApplication {
 
 		SpringApplication.run(AppwithunalApplication.class, args);
 	}
+
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
+		URL url = new URL("http://localhost:8080/api/messages");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+
+		in.close();
+		conn.disconnect();
+
+		// System.out.println("Message from server: " + content.toString());
+		return String.format("Message from server: " + content.toString());
 	}
 
 }
